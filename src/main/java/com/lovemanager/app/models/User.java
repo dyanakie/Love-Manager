d6eb4af9b52c9179a8737abf96edeff346db4e56
dyanakie <dyanakie@yahoo.com>
2018-07-26 16:57:02 +0300

    Added hibernate entity user

--- src/main/java/com/lovemanager/app/models/User.java
+++ src/main/java/com/lovemanager/app/models/User.java
@@ -1,4 +1,22 @@
 package com.lovemanager.app.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "LoveManager")
 public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column( name = "ID")
+    int id;
+
+    @Column( name = "UserName")
+    String username;
+
+    @Column( name = "Password")
+    String password;
+
+
 }
