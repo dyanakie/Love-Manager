0d2de319f65b5b20be1d69360feb7f4b074f3d68
dyanakie <dyanakie@yahoo.com>
2018-07-26 15:46:07 +0300

    First commit

---
+++ src/main/java/com/lovemanager/app/LovemanagerApplication.java
@@ -0,0 +1,12 @@
+package com.lovemanager.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovemanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovemanagerApplication.class, args);
+    }
+}
