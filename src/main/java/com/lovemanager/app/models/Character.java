dac6da94de3d56e662bce2a01756f1d3408054c5
dyanakie <dyanakie@yahoo.com>
2018-07-27 09:43:33 +0300

    created character entity

---
+++ src/main/java/com/lovemanager/app/models/Character.java
@@ -0,0 +1,52 @@
+package com.lovemanager.app.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "character")
+public class Character {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "level")
+    private int level;
+
+    @Column(name = "statsId")
+    private int statsId;
+
+    public Character(){
+
+    }
+
+    public Character(int level, int statsId) {
+        this.level = level;
+        this.statsId = statsId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getStatsId() {
+        return statsId;
+    }
+
+    public void setStatsId(int statsId) {
+        this.statsId = statsId;
+    }
+}
