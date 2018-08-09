6a860f78326328fa898874f1a0da66caa67d278f
dyanakie <dyanakie@yahoo.com>
2018-08-09 17:13:46 +0300

    added unit tests for encryption

--- src/test/java/com/lovemanager/app/services/PasswordEncryptionTests1.java
+++ src/test/java/com/lovemanager/app/services/PasswordEncryptionTests1.java
@@ -1,7 +1,36 @@
 package com.lovemanager.app.services;
 
+import com.lovemanager.app.service.PasswordEncryption;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class PasswordEncryptionTests1 {
+
+    @InjectMocks
+    PasswordEncryption passwordEncryption;
+
+    @Test
+    public void Test_Encrypted_Message_Same_As_Decrypted(){
+
+        String test = passwordEncryption.encryptPassword("PAROLA RIBA MECH");
+
+        Assert.assertEquals(passwordEncryption.decryptPassword(test), "PAROLA RIBA MECH");
+
+    }
+
+    @Test
+    public void Test_Encryption_Equals_Certain_String(){
+
+        String test = passwordEncryption.encryptPassword("123");
+
+        Assert.assertEquals(test, "022113$3");
+
+    }
+
+
 
 }
